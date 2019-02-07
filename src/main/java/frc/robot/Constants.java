7c5af7a99dc901f0e5c66128229e09f5a409423d
Sachitt Arora <sachittarora@icloud.com>
2019-02-06 18:42:17 -0800

    init commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+
+public class Constants{
+    public static final int DRIVE_PORT = 3;
+    
+    
+}
---
+++ src/main/java/frc/robot/DriveTrain.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class DriveTrain {
+    DifferentialDrive driveTrain;
+
+    
+}
